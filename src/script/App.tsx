b93ee67ed10523431b78d32ac5b4bcbef3a7fa29
EvgeniaM6 <evgminukova6@gmail.com>
2023-07-15 16:31:55 +0300

    feat: create header with navigation

--- src/script/App.tsx
+++ src/script/App.tsx
@@ -1,3 +1,36 @@
+import { Routes, Route } from 'react-router-dom';
+import { ConfigProvider } from 'antd';
+import { Layout } from 'antd';
+import { Welcome, Main, ErrorPage } from './pages';
+import { HeaderElem } from './components';
+
+const { Header, Content } = Layout;
+
 export const App = () => {
-  return <div>gg</div>;
+  return (
+    <>
+      <ConfigProvider
+        theme={{
+          token: {
+            colorPrimary: '#523857',
+          },
+        }}
+      >
+        <Layout>
+          <Header>
+            <HeaderElem />
+          </Header>
+          <Content>
+            <main className="main">
+              <Routes>
+                <Route path="/" element={<Welcome />} />
+                <Route path="/route" element={<Main />} />
+                <Route path="*" element={<ErrorPage />} />
+              </Routes>
+            </main>
+          </Content>
+        </Layout>
+      </ConfigProvider>
+    </>
+  );
 };
