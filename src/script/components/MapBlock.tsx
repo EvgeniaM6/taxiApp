66914e9c314e0921ce0ad2438cc578fa97e4a5d6
EvgeniaM6 <evgminukova6@gmail.com>
2023-07-26 10:55:14 +0300

    feat: add custom map marker

--- src/script/components/MapBlock.tsx
+++ src/script/components/MapBlock.tsx
@@ -1,6 +1,15 @@
-import { LatLngExpression, LatLngTuple } from 'leaflet';
+import { LatLngExpression, LatLngTuple, LeafletEventHandlerFnMap, DivIcon } from 'leaflet';
 import { useState } from 'react';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import { LocationMarker } from './LocationMarker';
+import { renderToString } from 'react-dom/server';
+import { EnvironmentFilled } from '@ant-design/icons';
+import { primaryAppColor } from '../../constants';
+
+const iconHtmlString = renderToString(
+  <EnvironmentFilled style={{ color: primaryAppColor, transform: 'scale(3.5)' }} />
+);
+const icon: DivIcon = new DivIcon({ html: iconHtmlString });
 
 export const MapBlock = () => {
   const mapStyle: React.CSSProperties = {
@@ -8,6 +17,8 @@ export const MapBlock = () => {
   };
 
   const [position, setPosition] = useState<LatLngExpression | null>(null);
+  const [startPoint, setStartPoint] = useState<LatLngExpression | null>(null);
+  const [finishPoint, setFinishPoint] = useState<LatLngExpression | null>(null);
 
   const succ: PositionCallback = (pos) => {
     const lat = pos.coords.latitude;
@@ -20,8 +31,15 @@ export const MapBlock = () => {
     const isSimilarPosition = (position as LatLngTuple)[0] === lat;
     if (isSimilarPosition) return;
     setPosition(newPosition);
+    setStartPoint(newPosition);
   };
   navigator.geolocation.getCurrentPosition(succ);
+
+  const dragLocation: LeafletEventHandlerFnMap = {
+    mouseup: (event) => {
+      setStartPoint(event.latlng);
+    },
+  };
 
   return (
     <>
@@ -31,13 +49,13 @@ export const MapBlock = () => {
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <Marker position={position}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
+          <Marker position={position} draggable eventHandlers={dragLocation} icon={icon}>
+            <Popup>From</Popup>
           </Marker>
+          <LocationMarker position={finishPoint} setPosition={setFinishPoint} />
         </MapContainer>
       )}
+      <EnvironmentFilled />
     </>
   );
 };
