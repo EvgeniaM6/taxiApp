f9fa10956019d41aa6471f2c6977e74135adae8e
EvgeniaM6 <evgminukova6@gmail.com>
2023-08-21 17:12:06 +0300

    fix: close popup when enter address to input element

--- src/script/components/map/StartPointMarker.tsx
+++ src/script/components/map/StartPointMarker.tsx
@@ -1,11 +1,11 @@
-import { LatLngExpression, LeafletEventHandlerFnMap, LeafletMouseEvent } from 'leaflet';
-import { Marker, Popup, useMap } from 'react-leaflet';
+import { LatLng, LatLngExpression, LeafletEventHandlerFnMap, LeafletMouseEvent } from 'leaflet';
+import { Marker, Popup, useMap, useMapEvents } from 'react-leaflet';
 import { useAppDispatch, useAppSelector } from '../../hooks';
-import { LocationPopup } from './LocationPopup';
 import { customMarker } from './customMarker';
 import { PRIMARY_APP_COLOR } from '../../../constants';
 import { setCanBuildRoute, setStartPoint } from '../../store/routeSlice';
 import { useEffect } from 'react';
+import L from 'leaflet';
 
 export const StartPointMarker = (props: { position: LatLngExpression }) => {
   const { position } = props;
@@ -24,9 +24,25 @@ export const StartPointMarker = (props: { position: LatLngExpression }) => {
     },
   };
 
+  useMapEvents({
+    popupclose() {
+      if (startPoint) return;
+      const { lat, lng } = position as LatLng;
+      dispatch(setStartPoint({ lat, lng }));
+      dispatch(setCanBuildRoute(true));
+    },
+  });
+
+  const popup = L.popup();
+  popup.setLatLng(position).setContent('You are here?');
+
   useEffect(() => {
-    if (!startPoint) return;
+    if (!startPoint) {
+      map.openPopup(popup);
+      return;
+    }
     map.flyTo(startPoint);
+    map.closePopup();
   }, [startPoint]);
 
   return (
@@ -43,7 +59,6 @@ export const StartPointMarker = (props: { position: LatLngExpression }) => {
       ) : (
         <>
           <Marker position={position} icon={customMarker(PRIMARY_APP_COLOR)} />
-          <LocationPopup position={position} />
         </>
       )}
     </>
